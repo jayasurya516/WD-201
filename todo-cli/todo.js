7629beb167cfe500df1dbcc581a6ececd2901229
jayasurya516 <141731513+jayasurya516@users.noreply.github.com>
2023-08-30 19:01:18 +0530

    Add files via upload

---
+++ todo-cli/todo.js
@@ -0,0 +1,55 @@
+const todoList = () => {
+    all = []
+    const add = (todoItem) => {
+      all.push(todoItem)
+    }
+    const markAsComplete = (index) => {
+      all[index].completed = true
+    }
+  
+    const overdue = () => {
+      // Write the date check condition here and return the array
+      // of overdue items accordingly.
+      let O_Result = [];
+      O_Result = all.filter((TodoList) => TodoList.dueDate < new Date().toLocaleDateString("en-CA"));
+      return OverdueResult;
+    }
+  
+    const dueToday = () => {
+      // Write the date check condition here and return the array
+      // of todo items that are due today accordingly.
+      let D_Result = [];
+      D_Result=all.filter((TodoList) => TodoList.dueDate === new Date().toLocaleDateString("en-CA"));
+      return DueTodayResult;
+    }
+  
+    const dueLater = () => {
+      // Write the date check condition here and return the array
+      // of todo items that are due later accordingly.
+      let DueLater = [];
+      DueLater = all.filter((TodoList) => TodoList.dueDate > new Date().toLocaleDateString("en-CA"));
+      return DueLaterResult;
+    }
+  
+    const toDisplayableList = (list) => {
+      // Format the To-Do list here, and return the output string
+      // as per the format given above.
+      return list.map(
+        (TodoList) =>
+          `[${TodoList.completed ? "x" : " "}] ${TodoList.title} ${
+            TodoList.dueDate !== formattedDate(new Date()) ? TodoList.dueDate : ""
+          }`).join("\n").trim();
+    }
+  
+    return {
+      all,
+      add,
+      markAsComplete,
+      overdue,
+      dueToday,
+      dueLater,
+      toDisplayableList
+    };
+  };
+  
+  module.exports = todoList;
